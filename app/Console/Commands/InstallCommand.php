d71da0af41f7466e7b928ddcf45504a8bbdc0c16
ntoufoudis <93659348+ntoufoudis@users.noreply.github.com>
2026-03-27 08:34:54 +0000

    fix code style

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Command;
+
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\intro;
 use function Laravel\Prompts\note;
